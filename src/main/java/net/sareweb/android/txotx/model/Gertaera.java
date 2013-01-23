540fceb18208e292e9729325a083a56126af758e
Aritz Galdos <galdos.aritz@gmail.com>
2013-01-23 20:52:38 +0100

    Txoooooootx

---
+++ src/main/java/net/sareweb/android/txotx/model/Gertaera.java
@@ -0,0 +1,86 @@
+package net.sareweb.android.txotx.model;
+
+import net.sareweb.lifedroid.model.generic.LDObject;
+
+public class Gertaera extends LDObject {
+
+	private long gertaeraId;
+	private long sagardotegiId;
+	private long userId;
+	private String screenName;
+	private String gertaeraMota;
+	private String testua;
+	private long irudiKarpetaId;
+	private String irudia;
+	private long balorazioa;
+	private long createDate;
+	private long modifiedDate;
+	
+	public long getGertaeraId() {
+		return gertaeraId;
+	}
+	public void setGertaeraId(long gertaeraId) {
+		this.gertaeraId = gertaeraId;
+	}
+	public long getSagardotegiId() {
+		return sagardotegiId;
+	}
+	public void setSagardotegiId(long sagardotegiId) {
+		this.sagardotegiId = sagardotegiId;
+	}
+	public long getUserId() {
+		return userId;
+	}
+	public void setUserId(long userId) {
+		this.userId = userId;
+	}
+	public String getScreenName() {
+		return screenName;
+	}
+	public void setScreenName(String screenName) {
+		this.screenName = screenName;
+	}
+	public String getGertaeraMota() {
+		return gertaeraMota;
+	}
+	public void setGertaeraMota(String gertaeraMota) {
+		this.gertaeraMota = gertaeraMota;
+	}
+	public String getTestua() {
+		return testua;
+	}
+	public void setTestua(String testua) {
+		this.testua = testua;
+	}
+	public long getIrudiKarpetaId() {
+		return irudiKarpetaId;
+	}
+	public void setIrudiKarpetaId(long irudiKarpetaId) {
+		this.irudiKarpetaId = irudiKarpetaId;
+	}
+	public String getIrudia() {
+		return irudia;
+	}
+	public void setIrudia(String irudia) {
+		this.irudia = irudia;
+	}
+	public long getBalorazioa() {
+		return balorazioa;
+	}
+	public void setBalorazioa(long balorazioa) {
+		this.balorazioa = balorazioa;
+	}
+	public long getCreateDate() {
+		return createDate;
+	}
+	public void setCreateDate(long createDate) {
+		this.createDate = createDate;
+	}
+	public long getModifiedDate() {
+		return modifiedDate;
+	}
+	public void setModifiedDate(long modifiedDate) {
+		this.modifiedDate = modifiedDate;
+	}
+	
+}
---
+++ src/main/java/net/sareweb/android/txotx/model/Sagardotegi.java
@@ -0,0 +1,120 @@
+package net.sareweb.android.txotx.model;
+
+import net.sareweb.lifedroid.model.generic.LDObject;
+
+public class Sagardotegi extends LDObject {
+
+	private long sagardotegiId;
+	private String izena;
+	private String herria;
+	private String helbidea;
+	private String telefonoa;
+	private String emaila;
+	private String weborria;
+	private double lat;
+	private double lng;
+	private long edukiera;
+	private long irudiKarpetaId;
+	private String irudia;
+	private long balorazioKopurua;
+	private double balorazioenBB;
+	private long createDate;
+	private long modifiedDate;
+	public long getSagardotegiId() {
+		return sagardotegiId;
+	}
+	public void setSagardotegiId(long sagardotegiId) {
+		this.sagardotegiId = sagardotegiId;
+	}
+	public String getIzena() {
+		return izena;
+	}
+	public void setIzena(String izena) {
+		this.izena = izena;
+	}
+	public String getHerria() {
+		return herria;
+	}
+	public void setHerria(String herria) {
+		this.herria = herria;
+	}
+	public String getHelbidea() {
+		return helbidea;
+	}
+	public void setHelbidea(String helbidea) {
+		this.helbidea = helbidea;
+	}
+	public String getTelefonoa() {
+		return telefonoa;
+	}
+	public void setTelefonoa(String telefonoa) {
+		this.telefonoa = telefonoa;
+	}
+	public String getEmaila() {
+		return emaila;
+	}
+	public void setEmaila(String emaila) {
+		this.emaila = emaila;
+	}
+	public String getWeborria() {
+		return weborria;
+	}
+	public void setWeborria(String weborria) {
+		this.weborria = weborria;
+	}
+	public double getLat() {
+		return lat;
+	}
+	public void setLat(double lat) {
+		this.lat = lat;
+	}
+	public double getLng() {
+		return lng;
+	}
+	public void setLng(double lng) {
+		this.lng = lng;
+	}
+	public long getEdukiera() {
+		return edukiera;
+	}
+	public void setEdukiera(long edukiera) {
+		this.edukiera = edukiera;
+	}
+	public long getIrudiKarpetaId() {
+		return irudiKarpetaId;
+	}
+	public void setIrudiKarpetaId(long irudiKarpetaId) {
+		this.irudiKarpetaId = irudiKarpetaId;
+	}
+	public String getIrudia() {
+		return irudia;
+	}
+	public void setIrudia(String irudia) {
+		this.irudia = irudia;
+	}
+	public long getBalorazioKopurua() {
+		return balorazioKopurua;
+	}
+	public void setBalorazioKopurua(long balorazioKopurua) {
+		this.balorazioKopurua = balorazioKopurua;
+	}
+	public double getBalorazioenBB() {
+		return balorazioenBB;
+	}
+	public void setBalorazioenBB(double balorazioenBB) {
+		this.balorazioenBB = balorazioenBB;
+	}
+	public long getCreateDate() {
+		return createDate;
+	}
+	public void setCreateDate(long createDate) {
+		this.createDate = createDate;
+	}
+	public long getModifiedDate() {
+		return modifiedDate;
+	}
+	public void setModifiedDate(long modifiedDate) {
+		this.modifiedDate = modifiedDate;
+	}
+	
+}
