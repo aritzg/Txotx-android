2923fbeeb682b43ad88a1c0a7114fa7bc54fc27d
Aritz Galdos <galdos.aritz@gmail.com>
2013-02-26 21:23:13 +0100

    #51 @izena jartzean pertsonari push notifikazioa

--- src/main/java/net/sareweb/android/txotx/activity/SagardotegiDetailActivity.java
+++ src/main/java/net/sareweb/android/txotx/activity/SagardotegiDetailActivity.java
@@ -1,40 +1,84 @@
 package net.sareweb.android.txotx.activity;
 
+import java.util.List;
+
 import net.sareweb.android.txotx.R;
+import net.sareweb.android.txotx.adapter.SagardotegiAdapter;
+import net.sareweb.android.txotx.cache.SagardotegiCache;
 import net.sareweb.android.txotx.fragment.SagardotegiDetailFragment;
 import net.sareweb.android.txotx.listener.tab.SagardotegiDetailTabListener;
 import net.sareweb.android.txotx.model.Sagardotegi;
+import net.sareweb.android.txotx.util.TxotxPrefs_;
+import android.app.NotificationManager;
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.util.Log;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.ActionBar.Tab;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.googlecode.androidannotations.annotations.Background;
 import com.googlecode.androidannotations.annotations.EActivity;
 import com.googlecode.androidannotations.annotations.Extra;
 import com.googlecode.androidannotations.annotations.FragmentById;
 import com.googlecode.androidannotations.annotations.OptionsItem;
 import com.googlecode.androidannotations.annotations.OptionsMenu;
+import com.googlecode.androidannotations.annotations.UiThread;
+import com.googlecode.androidannotations.annotations.sharedpreferences.Pref;
 
 @EActivity(R.layout.sagardotegi_detail_activity)
 public class SagardotegiDetailActivity extends SherlockFragmentActivity{
 
 	private static String TAG = "SagardotegiDetailActivity";
+	@Pref TxotxPrefs_ prefs;
 	@Extra Sagardotegi sagardotegi;
+	@Extra long sagardotegiId; //used whe comming from notification
+	private ProgressDialog dialog;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		Log.d(TAG,"onCreate");
 		
-		/*if(savedInstanceState!=null){
-			//Workarond: Pantaila giratu da eta fragment kabiatuek erroreak ematen dituzte.
-			finish();
-			return;
-		}*/
-		
+		if(sagardotegiId!=0){
+			//close notification
+			NotificationManager mNotificationManager =(NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+			mNotificationManager.cancel((int)sagardotegiId);
+			
+			dialog = ProgressDialog.show(this, "", "Sagardotegia eskuratzen...", true);
+			dialog.show();
+			getSagardotegiak(true);
+		}else{
+			marraztuPantaila();
+		}
+
+	}
+	
+	@Background
+	public void getSagardotegiak(boolean refresh){
+		Log.d(TAG, "Gettings sagardotegiak");
+		SagardotegiCache.init(prefs);
+		getSagardotegiakResult(SagardotegiCache.getSagardotegiak(refresh));
+	}
+
+	@UiThread
+	public void getSagardotegiakResult(List<Sagardotegi> sagardotegiak){
+		for(Sagardotegi sagardotegiTmp : sagardotegiak){
+			if(sagardotegiTmp.getSagardotegiId()==sagardotegiId){
+				sagardotegi=sagardotegiTmp;
+				break;
+			}
+		}
+		marraztuPantaila();
+		dialog.cancel();
+	}
+	
+	private void  marraztuPantaila(){
 		ActionBar actionBar = getSupportActionBar();
 		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 		actionBar.setDisplayHomeAsUpEnabled(true);
@@ -50,37 +94,20 @@ public class SagardotegiDetailActivity extends SherlockFragmentActivity{
 		gardenEventsTab.setTabListener(new SagardotegiDetailTabListener(sagardotegi, SagardotegiDetailTabListener.SAGARDOTEGI_GERTAERAK, this));
 		actionBar.addTab(gardenEventsTab);
 
-		actionBar.setSelectedNavigationItem(0);
-	}
-
-	@Override
-	protected void onResume() {
-		Log.d(TAG,"onResume");
-		super.onResume();
-		//gardenDetailFragment.setGardenContent(garden);
-	}
-	
-	@Override
-	protected void onPause() {
-		super.onPause();
-		Log.d(TAG,"onPause");
-	}
-	
-	@Override
-	protected void onStop() {
-		super.onStop();
-		Log.d(TAG,"onStop");
-	}
-	
-	@Override
-	protected void onDestroy() {
-		super.onDestroy();
-		Log.d(TAG,"onDestry");
+		if(sagardotegiId!=0){
+			actionBar.setSelectedNavigationItem(1);
+		}
+		else{
+			actionBar.setSelectedNavigationItem(0);	
+		}
 	}
 
 	@OptionsItem
 	void homeSelected() {
 		finish();
+		if(sagardotegiId!=0){
+			SagardotegiakActivity_.intent(this).start();
+		}
 	}
 
 
--- src/main/java/net/sareweb/android/txotx/notification/TxotxNotifications.java
+++ src/main/java/net/sareweb/android/txotx/notification/TxotxNotifications.java
@@ -1,20 +1,25 @@
 package net.sareweb.android.txotx.notification;
 
 import net.sareweb.android.txotx.R;
+import net.sareweb.android.txotx.activity.DashboardActivity_;
+import net.sareweb.android.txotx.activity.LogInActivity_;
+import net.sareweb.android.txotx.activity.SagardotegiDetailActivity_;
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationCompat.Builder;
+import android.support.v4.app.TaskStackBuilder;
 
 public class TxotxNotifications {
 	
 	public static void showDeviceResgistration(Context context, Intent intent){
 		 Builder builder = new NotificationCompat.Builder(context);
-		 builder.setContentTitle("Txootx!");
+		 builder.setContentTitle("Txootx! aipuak");
 		 builder.setSmallIcon(R.drawable.notification);
-		 builder.setContentText("Gogoan izan iruzkinetan @izena jarri ezkero, pertsona horri oharra iritsiko zaiola. Txootx!");
+		 builder.setContentText("@izena jarriz aipuak egin daitezke!");
 		 Notification notification = builder.build();
 		
 		 NotificationManager mNotificationManager =
@@ -25,16 +30,33 @@ public class TxotxNotifications {
 	public static void showMention(Context context, Intent intent){
 		String testua = intent.getExtras().getString("testua");
 		String sagardotegiIzena = intent.getExtras().getString("sagardotegiIzena");
+		String sagardotegiId = intent.getExtras().getString("sagardotegiId");
+		long sagardotegiIdLng = Long.parseLong(sagardotegiId);
+		
 		String nork = intent.getExtras().getString("nork");
+		
 		 Builder builder = new NotificationCompat.Builder(context);
-		 builder.setContentTitle("Txootx! " + nork +"-(e)k " + sagardotegiIzena + "-(e)n");
+		 builder.setContentTitle(nork +"(e)k " + sagardotegiIzena + "(e)n");
 		 builder.setSmallIcon(R.drawable.notification);
 		 builder.setContentText(testua);
+		 
+		 Intent detailIntent = SagardotegiDetailActivity_.intent(context).sagardotegiId(sagardotegiIdLng).get();
+		 
+		 TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+		 stackBuilder.addParentStack(SagardotegiDetailActivity_.class);
+		 stackBuilder.addNextIntent(detailIntent);
+		 PendingIntent resultPendingIntent =
+			        stackBuilder.getPendingIntent(
+			            0,
+			            PendingIntent.FLAG_UPDATE_CURRENT
+			        );
+		 builder.setContentIntent(resultPendingIntent);
+		 
 		 Notification notification = builder.build();
 		
 		 NotificationManager mNotificationManager =
 			        (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-		 mNotificationManager.notify(0,notification);
+		 mNotificationManager.notify((int)sagardotegiIdLng, notification);
 	}
 
 }
